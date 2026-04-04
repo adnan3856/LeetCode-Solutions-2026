22f9d7566227af0807dd42534fc72f9935879997
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-04-04 10:38:29 +0530

    Update Solution.java

--- 345. Reverse Vowels of a String/Solution.java
+++ 345. Reverse Vowels of a String/Solution.java
@@ -4,7 +4,7 @@ class Solution {
         int i=0, j=s.length()-1;
         char[] str = s.toCharArray();
         while(i < j){
-            if("aeiouAEIOU".indexOf(s.charAt(i)) == -1 )
+            if("aeiouAEIOU".indexOf(s.charAt(i)) == -1 )  // this takes extra time indexOf
                 i++;
             if("aeiouAEIOU".indexOf(s.charAt(j)) == -1 )
                 j--;
@@ -17,5 +17,33 @@ class Solution {
             }      
         }
         return new String(str);
+    }
+}
+
+
+// Another solution - faster than above approach
+class Solution {
+    public String reverseVowels(String s) {
+        int i=0, j=s.length()-1;
+        char[] str = s.toCharArray();
+        while(i < j){
+            if( !isVowel(s.charAt(i)))
+                i++;
+            if( !isVowel(s.charAt(j)))
+                j--;
+            if(isVowel(s.charAt(i))== true && isVowel(s.charAt(j))== true ){
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+                i++;
+                j--;
+            }      
+        }
+        return new String(str);
+    }
+
+    private boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
     }
 }
