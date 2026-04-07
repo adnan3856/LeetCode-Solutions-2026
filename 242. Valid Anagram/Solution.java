fe4f7dbb1e24d11c140515305b1938f4363af90f
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2026-04-08 00:01:28 +0530

    Create Solution.java

---
+++ 242. Valid Anagram/Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int [] alphabets = new int[26];
+        if(s.length() != t.length())
+            return false;
+        for(int i=0; i< s.length(); i++) {
+            alphabets[s.charAt(i) - 'a'] ++;
+        }
+        for(int i=0; i< s.length(); i++) {
+            alphabets[t.charAt(i) - 'a'] --;
+        }
+
+        for(int count : alphabets){
+            if(count > 0)
+                return false;
+        }
+        return true;
+    }
+}
+
+
+// Question
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "rat", t = "car"
+
+// Output: false
+
+ 
+
+// Constraints:
+
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+ 
+
+// Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
